e85b53a194de16aa71624e6a10ac8834cb7d2572
Joshua Gleitze <dev@joshuagleitze.de>
2020-09-29 14:16:32 +0200

    build: reduce dependencies on atrium

--- api-spec/build.gradle.kts
+++ api-spec/build.gradle.kts
@@ -11,6 +11,5 @@ dependencies {
 	implementation(project(":translations:${rootProject.name}-translation-en"))
 	implementation(name = "atrium-fluent-en_GB", version = atriumVersion, group = "ch.tutteli.atrium")
 	implementation(name = "spek-dsl-jvm", version = spekVersion, group = "org.spekframework.spek2")
-	implementation(name = "spek-runtime-jvm", version = spekVersion, group = "org.spekframework.spek2")
 	implementation(name = "spek-testfiles", version = "1.0.1", group = "de.joshuagleitze")
 }
