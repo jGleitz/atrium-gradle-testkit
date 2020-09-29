3c025d4f143260f10ce0027fe432722b7a739e2d
Joshua Gleitze <dev@joshuagleitze.de>
2020-09-29 16:50:07 +0200

    build: put common dependency versions into gradle.properties

--- translations/en/build.gradle.kts
+++ translations/en/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 dependencies {
-	val atriumVersion = "0.13.0"
+	val atriumVersion: String by project
 
 	implementation(gradleTestKit())
 	implementation(name = "atrium-core-api", version = atriumVersion, group = "ch.tutteli.atrium")
